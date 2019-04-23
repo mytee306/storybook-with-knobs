990f982dfb5e746ca5329f2d3d1145b63f6c3680
mateja <mateja176@gmail.com>
2019-04-23 14:17:15 +0200

    Returns key and knob wrapped value

--- src/utils/mockObjectToKnobs.ts
+++ src/utils/mockObjectToKnobs.ts
@@ -1,6 +1,6 @@
 import * as knobs from '@storybook/addon-knobs';
 import { startCase } from 'lodash';
-import { fromPairs, pipe } from 'ramda';
+import { fromPairs, pipe, toPairs } from 'ramda';
 
 type Value = string | number | boolean | Array<{}> | Object | Function;
 
@@ -12,34 +12,33 @@ type MockPairs = MockPair[];
 
 const types = ['number', 'string', 'boolean', 'object', 'function'];
 
-const prepareMockObject = (mockObject: MockObject): MockPairs =>
-  Object.entries(mockObject).map(([key, value]) => [startCase(key), value]);
-
 const filterMockArray = (mockPair: MockPairs) =>
   mockPair.filter(([, value]) => types.includes(typeof value));
 
-const mockPairToKnobs = ([label, value]: MockPair) => {
+const mockPairToKnobs = ([key, value]: MockPair) => {
   const type = typeof value;
 
+  const label = startCase(key);
+
   if (typeof value === 'string' && type.startsWith('#')) {
-    return knobs.color(label, value);
+    return [key, knobs.color(label, value)];
   } else if (typeof value === 'string') {
-    return knobs.text(label, value);
+    return [key, knobs.text(label, value)];
   } else if (Array.isArray(value)) {
-    return knobs.array(label, value);
+    return [key, knobs.array(label, value)];
   } else if (type === 'object' && value instanceof Date) {
-    return knobs['date'](label, value);
+    return [key, new Date(knobs.date(label, value))];
   } else {
-    return (knobs as any)[type](label, value);
+    return [key, (knobs as any)[type](label, value)];
   }
 };
 
 const mockArrayToKnobs = (mockPairs: MockPairs) =>
   mockPairs.map(mockPairToKnobs);
 
-export default pipe(
-  prepareMockObject,
+export default (pipe as any)(
+  toPairs,
   filterMockArray,
   mockArrayToKnobs,
-  fromPairs as any,
+  fromPairs,
 );

707e5e00b402e6c3b28c4633bb9845f47bc63d6e
mateja <mateja176@gmail.com>
2019-04-23 13:20:27 +0200

    App displays mock perosnal information

--- src/App.story.tsx
+++ src/App.story.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from './App';
 import { storiesOf } from '@storybook/react';
+import { personalInformation } from './mocks/personalInformation';
 
-storiesOf('App', module).add('Default', () => <App />);
+storiesOf('App', module).add('Default', () => <App {...personalInformation} />);
--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface AppProps {
+  name: string;
+  age: number;
+  birthday: Date;
+  employed: boolean;
+  hobbies: string[];
+  languages: { [language: string]: string };
 }
+
+const App: React.FC<AppProps> = ({
+  name,
+  age,
+  birthday,
+  employed,
+  hobbies,
+  languages,
+}) => {
+  return (
+    <form className="App">
+      <label>
+        Name:
+        <input type="text" value={name} />
+      </label>
+      <label>
+        Age:
+        <input type="text" value={age} />
+      </label>
+      <label>
+        Employed:
+        <input type="text" value={JSON.stringify(employed)} />
+      </label>
+      <label>
+        Hobbies:
+        <input type="text" value={hobbies} />
+      </label>
+      <fieldset>
+        {Object.entries(languages).map(([language, level]) => (
+          <label>
+            {language}:
+            <input value={level} />
+          </label>
+        ))}
+      </fieldset>
+      <label>
+        Birthday:
+        <input type="text" value={birthday.toLocaleDateString()} />
+      </label>
+    </form>
+  );
+};
 
 export default App;

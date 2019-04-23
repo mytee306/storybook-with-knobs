78706b7889b0e9458e38de5b4daf2480eeab1e42
mateja <mateja176@gmail.com>
2019-04-23 12:24:38 +0200

    Adds mock object

---
+++ src/mocks/personalInformation.ts
@@ -0,0 +1,12 @@
+export const personalInformation = {
+  name: 'John',
+  age: 25,
+  employed: true,
+  favoriteColor: '#eee',
+  hobbies: ['Running', 'Reading'],
+  languages: {
+    english: 'Mother Language',
+    german: 'Proficient',
+  },
+  birthday: new Date('Jan 1 1994'),
+};

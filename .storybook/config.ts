3c71d480b06c62f973f480d9e08dfebcc04e393d
mateja <mateja176@gmail.com>
2019-04-23 14:18:57 +0200

    Registers knobs and adds global decorator

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,10 +1,12 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
 
 const requireContext = require.context('../src', true, /\.story\.tsx$/);
 
 function loadStories() {
   addDecorator(withInfo);
+  addDecorator(withKnobs);
 
   requireContext.keys().forEach(requireContext);
 }

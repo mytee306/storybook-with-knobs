bf025638b0a9ed34036db63be268e0e58b6dd0c6
Mateja Petrovic <ma.petrovic@levi9.com>
2019-05-28 15:40:48 +0200

    removes comment

--- src/utils/withKnobs.ts
+++ src/utils/withKnobs.ts
@@ -8,7 +8,6 @@ type MockEntries = MockEntry[];
 // for more information check out https://www.debuggex.com/r/vpn4CGZFfeN7WR1_
 const dateRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})/;
 
-// knobs does not have an index signature
 const mockEntryToKnobs = ([key, value]: MockEntry): MockEntry => {
   const label = startCase(key);
 
